75d159a8f0a8ebbd67697993a357af4b879a341f
YuriScian <yuri.scian@ialedu.it>
2023-03-22 16:11:35 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Fiera_20._03._2023
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trbRed = new System.Windows.Forms.TrackBar();
+            this.trbGreen = new System.Windows.Forms.TrackBar();
+            this.trbBlue = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(32, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(483, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(820, 573);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trbRed
+            // 
+            this.trbRed.Location = new System.Drawing.Point(81, 131);
+            this.trbRed.Maximum = 255;
+            this.trbRed.Minimum = 1;
+            this.trbRed.Name = "trbRed";
+            this.trbRed.Size = new System.Drawing.Size(275, 45);
+            this.trbRed.TabIndex = 2;
+            this.trbRed.Value = 1;
+            this.trbRed.Scroll += new System.EventHandler(this.changeColor);
+            // 
+            // trbGreen
+            // 
+            this.trbGreen.Location = new System.Drawing.Point(81, 182);
+            this.trbGreen.Maximum = 255;
+            this.trbGreen.Minimum = 1;
+            this.trbGreen.Name = "trbGreen";
+            this.trbGreen.Size = new System.Drawing.Size(273, 45);
+            this.trbGreen.TabIndex = 3;
+            this.trbGreen.Value = 1;
+            this.trbGreen.Scroll += new System.EventHandler(this.changeColor);
+            // 
+            // trbBlue
+            // 
+            this.trbBlue.Location = new System.Drawing.Point(81, 233);
+            this.trbBlue.Maximum = 255;
+            this.trbBlue.Minimum = 1;
+            this.trbBlue.Name = "trbBlue";
+            this.trbBlue.Size = new System.Drawing.Size(273, 45);
+            this.trbBlue.TabIndex = 4;
+            this.trbBlue.Value = 1;
+            this.trbBlue.Scroll += new System.EventHandler(this.changeColor);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 194);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Blue";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Livelli di RGB";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1804, 805);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trbBlue);
+            this.Controls.Add(this.trbGreen);
+            this.Controls.Add(this.trbRed);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenFileDialog openFileDialog1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private TrackBar trbRed;
+        private TrackBar trbGreen;
+        private TrackBar trbBlue;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
